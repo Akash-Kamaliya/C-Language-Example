4e65d5437f1982782261485b5711de947578bd1c
Akash-Kamaliya <akashkamaliya112@gmail.com>
2024-11-26 10:58:22 +0530

    format the program

--- Lab Wise Example/lab-10/Armstrong.c
+++ Lab Wise Example/lab-10/Armstrong.c
@@ -3,16 +3,16 @@
 
 int main()
 {
-  int n, n1, r, r1=0,sum = 0;
+  int n, n1, r, r1 = 0, sum = 0;
   printf("Enter a number :\n");
   scanf("%d", &n1);
-  n= n1;
+  n = n1;
   while (n > 0)
   {
-    r1 = r1* 10 + n % 10;
+    r1 = r1 * 10 + n % 10;
     n = n / 10;
   }
-  n1=r1;
+  n1 = r1;
   while (n1 != 0)
   {
     r = n1 % 10;

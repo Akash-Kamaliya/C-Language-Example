032a81e95c4068a1a335693c5f69f0716bc9d17c
Akash-Kamaliya <akashkamaliya112@gmail.com>
2024-11-28 22:41:14 +0530

    add program

--- Topic Wise Example/LeetCode/demo.c
+++ Topic Wise Example/LeetCode/demo.c
@@ -1,34 +1,15 @@
-// write a c program of Swap two numbers using call by value and call by reference and print them.
-
 #include <stdio.h>
-
-void swapByValue(int a, int b)
-{
-    int temp = a;
-    a = b;
-    b = temp;
-}
-
-void swapByReference(int *a, int *b)
-{
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
 
 int main()
 {
-    int num1 = 10, num2 = 20;
-    printf("Before swapping: num1 = %d, num2 = %d\n", num1, num2);
 
-    // Swap by value
-
-    swapByValue(num1, num2);
-    printf("After swapping by value: num1 = %d, num2 = %d\n", num1, num2);
-
-    // Swap by reference
-    swapByReference(&num1, &num2);
-    printf("After swapping by reference: num1 = %d, num2 = %d\n", num1, num2);
-
+    char s[100];
+    fgets(s, 100, stdin);
+    printf("Hello World!\n");
+    fputs(s,stdout);
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
     return 0;
 }

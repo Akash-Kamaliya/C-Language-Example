0b20cfb94737146802c5848b946371d66f504020
Akash-Kamaliya <akashkamaliya112@gmail.com>
2025-05-06 11:42:56 +0530

    edit some program

--- Topic Wise Example/4LoopControlInstruction/RverseDigits.c
+++ Topic Wise Example/4LoopControlInstruction/RverseDigits.c
@@ -7,8 +7,8 @@ int main()
   scanf("%d",&n);
   while (n > 0)
     {
-      revnum = revnum * 10 + n % 10; 
-      n = n / 10; 
+      revnum *= 10 + n % 10; 
+      n /= 10; 
     }
   printf("The Reversed Number is : %d\n", revnum);
   return 0;
--- Topic Wise Example/7Pointers/ArrayOP.c
+++ Topic Wise Example/7Pointers/ArrayOP.c
@@ -5,17 +5,16 @@ int main()
   int a[3][3] = {{11, 22, 33}, {44, 55, 66}, {77, 88, 99}};
   int b[5] = {1, 2, 3, 4, 5};
 
-  // printf("%d", *(b + 1));
-  // printf("%d", *(*(a + 1) + 1));
+  printf("%d\n", *(b + 1));
+  printf("%d\n", *(*(a + 1) + 1));
 
   char city[3][25] = {"Akash", "Sandip", "Het"};
-  // printf("%d", &city[0]);
-  // printf("%d", &city[1]);
-  // printf("%d", &city[1]);
+  printf("%d\n", &city[0]);
+  printf("%d\n", &city[1]);
+  printf("%d\n", &city[1]);
 
-  // printf("%d", (city[0] + 2));
-  printf("%s", (*(city[0] )));
-  // printf("%d", *(city[0] + 2) + 3);
+  printf("%d\n", (city[0] + 2));
+  printf("%d\n", *(city[0] + 2) + 3);
 
   return 0;
 }

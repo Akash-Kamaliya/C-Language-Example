3ef87a2e86c744847d1b02ac26daf9e09e9f32b5
Akash-Kamaliya <akashkamaliya112@gmail.com>
2025-06-15 19:33:20 +0530

    Update program

--- DSA/Lab-4/05DeleteDublicate.c
+++ DSA/Lab-4/05DeleteDublicate.c
@@ -1,38 +1,33 @@
-// Write a program to delete duplicate numbers from an array.
 #include <stdio.h>
 
-int main()
-{
-    int n;
-    printf("Enter the size of the array: ");
+int main() {
+    int arr[100], n, i, j, k;
+
+    printf("Enter the number of elements: ");
     scanf("%d", &n);
-    int nums[n];
-    printf("Enter %d elements in non-decreasing order: \n", n);
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%d", &nums[i]);
+
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
-    if (n == 0)
-    {
-        printf("Number of unique elements: 0\n");
-        return 0;
-    }
-    int j = 0;
-    for (int i = 1; i < n; i++)
-    {
-        if (nums[i] != nums[j])
-        {
-            j++;
-            nums[j] = nums[i];
+
+    for(i = 0; i < n; i++) {
+        for(j = i + 1; j < n; ) {
+            if(arr[i] == arr[j]) {
+                for(k = j; k < n - 1; k++) {
+                    arr[k] = arr[k + 1];
+                }
+                n--; 
+            } else {
+                j++;
+            }
         }
     }
-    int k = j + 1;
-    printf("Number of unique elements: %d\n", k);
-    printf("Array after removing duplicates: ");
-    for (int i = 0; i < k; i++)
-    {
-        printf("%d ", nums[i]);
+
+    printf("Array after removing duplicates:\n");
+    for(i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
     }
-    printf("\n");
+
     return 0;
 }

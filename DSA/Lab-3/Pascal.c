bf69aa3359f372b8690052a048191040967c40fe
Akash-Kamaliya <akashkamaliya112@gmail.com>
2025-06-15 19:12:07 +0530

    edit some program

--- DSA/Lab-3/Pascal.c
+++ DSA/Lab-3/Pascal.c
@@ -1,14 +1,21 @@
-/*Pascal Triangle*/
 #include <stdio.h>
 
-int main()
-{
-  int rows, coefficient = 1;
-  printf("Enter the number of rows for Pascal's Triangle: ");
-  scanf("%d", &rows);
-  for (int i = 0; i < rows; i++)
-  {
-    
-  }
-  return 0;
+int main() {
+    int n;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        int value = 1;
+        for (int j = 0; j < n - i - 1; j++)
+            printf("  ");
+
+        for (int j = 0; j <= i; j++) {
+            printf("%4d", value);
+            value = value * (i - j) / (j + 1);
+        }
+        printf("\n");
+    }
+    return 0;
 }
